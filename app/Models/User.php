01b63638443a488be5f1e3b9e232ca50e24e1e3e
jacee <contact@jacee.dev>
2024-07-17 19:01:27 +0200

    Add db tables for all required functions
    - add migrations and models for:
        - Dozenten
        - Studiengänge
        - Kurse
        - Stunden
        - Blockierte Zeiten

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -48,5 +49,10 @@ class User extends Authenticatable
             'password' => 'hashed',
             'admin' => 'boolean',
         ];
+    }
+
+    public function dozent(): HasOne // only one dozent can be assigned to a user
+    {
+        return $this->hasOne(Dozent::class);
     }
 }
